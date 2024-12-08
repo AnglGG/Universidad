42293351db2aa0aab0001921b28f87bfbfe1b2b8
Angel <angelgallego999@gmail.com>
2024-12-08 19:52:38 +0100

    Tarea 5 casi hecha (error en la redireccion <)

--- Sistemas_Operativos/Shell/Shell_project.c
+++ Sistemas_Operativos/Shell/Shell_project.c
@@ -1,5 +1,6 @@
 #include "job_control.h" // Incluye job_control.h para las macros y funciones relacionadas
 #include <time.h>
+#include <fcntl.h>
 
 #define MAX_LINE 256 /* Tamaño máximo para el buffer de entrada */
 
@@ -23,36 +24,30 @@ void manejador_child(int signal)
 		pid_wait = waitpid(-1, &w_status, WUNTRACED | WNOHANG | WCONTINUED);
 		if (pid_wait == 0) return; // No hay ningún cambio de los solicitados
 		if (pid_wait == -1) return;
+		block_SIGCHLD();
 		child = get_item_bypid(tasks, pid_wait);
 		// command = strdup(child->command);
 		if (child)
 		{
 			if (WIFSTOPPED(w_status))
-				{
-					block_SIGCHLD();
-					child->state = STOPPED;
-					unblock_SIGCHLD();
-					printf("Background pid: %d, Suspended, info: %d\n", child->pgid, WSTOPSIG(w_status));
-				}else if (WIFCONTINUED(w_status))
-				{
-					block_SIGCHLD();
-					child->state = BACKGROUND;
-					unblock_SIGCHLD();
-					printf("Background pid: %d, Continued, info: 0\n", child->pgid);
-				}else if (WIFSIGNALED(w_status))
-				{
-					block_SIGCHLD();
-					delete_job(tasks, child);
-					unblock_SIGCHLD();
-					printf("Background pid: %d, Signaled, info: %d\n", pid_wait, WTERMSIG(w_status));
-				}else
-				{
-					block_SIGCHLD();
-					delete_job(tasks, child);
-					unblock_SIGCHLD();
-					printf("Background pid: %d, Exited, info: %d\n", pid_wait, WEXITSTATUS(w_status));
-				}
+			{
+				child->state = STOPPED;
+				printf("Background pid: %d, Suspended, info: %d\n", child->pgid, WSTOPSIG(w_status));
+			}else if (WIFCONTINUED(w_status))
+			{
+				child->state = BACKGROUND;
+				printf("Background pid: %d, Continued, info: 0\n", child->pgid);
+			}else if (WIFSIGNALED(w_status))
+			{
+				delete_job(tasks, child);
+				printf("Background pid: %d, Signaled, info: %d\n", pid_wait, WTERMSIG(w_status));
+			}else
+			{
+				delete_job(tasks, child);
+				printf("Background pid: %d, Exited, info: %d\n", pid_wait, WEXITSTATUS(w_status));
+			}
 		}
+		unblock_SIGCHLD();
 	}
 }
 
@@ -73,6 +68,9 @@ int main(void) {
 	int etime = 0;
 	/*Comando etime*/
 	struct timespec start_time, end_time;
+
+	int original_stdin = dup(STDIN_FILENO);  // Guardar descriptor original de stdin
+	int original_stdout = dup(STDOUT_FILENO); // Guardar descriptor original de stdout
 
 	ignore_terminal_signals();
 	signal(SIGCHLD, manejador_child);
@@ -171,6 +169,62 @@ int main(void) {
 			args[i-1] = args[i];
 			printf("Detectado etime\n");
 			clock_gettime(CLOCK_MONOTONIC, &start_time);
+		}else
+		{
+			int i = 0;
+			int input_fd = -1;
+			int output_fd = -1;
+			while (args[i])
+			{
+				if (strcmp(args[i], "<") == 0)
+				{
+					if (args[i + 1] == NULL)
+					{
+						fprintf(stderr, "Error: Redirección de entrada incompleta.\n");
+            			exit(EXIT_FAILURE);
+					}
+					input_fd = open(args[i + 1], O_RDONLY);
+					if (input_fd < 0)
+					{
+						fprintf(stderr, "Error al abrir el archivo de entrada.\n");
+						exit (EXIT_FAILURE);
+					}
+					dup2(input_fd, STDIN_FILENO);
+					close (input_fd);
+					int j = i;
+					while (args[j + 2] != NULL)
+					{
+						args[j] = args[j + 2];
+						j ++;
+					}
+					args[j] = NULL;
+					i --;
+				}else if (strcmp(args[i], ">" ) == 0)
+				{
+					if (args[i + 1] == NULL)
+					{
+						fprintf(stderr, "Error: Redirección de salida incompleta.\n");
+            			exit(EXIT_FAILURE);
+					}
+					output_fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					if (output_fd < 0)
+					{
+						fprintf(stderr, "Error al abrir el archivo de salida.\n");
+						exit (EXIT_FAILURE);
+					}
+					dup2(output_fd, STDOUT_FILENO);
+					close (output_fd);
+					int j = i;
+					while (args[j + 2] != NULL)
+					{
+						args[j] = args[j + 2];
+						j ++;
+					}
+					args[j] = NULL;
+					i --;
+				}
+				i ++;
+			}
 		}
 
 
@@ -216,6 +270,7 @@ int main(void) {
 				unblock_SIGCHLD();
 				printf("Background job running... pid: %d, command: %s\n", pid_fork, args[0]);
 				fg = 0;
+				
 			
 			}else // Foreground
 			{
@@ -224,6 +279,10 @@ int main(void) {
 				tcsetpgrp(STDIN_FILENO, group);
 				pid_wait = waitpid(pid_fork, &w_status, WUNTRACED); // Esperar al hijo (incluir WUNTRACED para detectar procesos suspendidos)
 				tcsetpgrp(STDIN_FILENO, getpgid( getpid())); // Recuperar el terminal para el shell
+				dup2(original_stdin, STDIN_FILENO);
+				dup2(original_stdout, STDOUT_FILENO);
+				close(original_stdin);
+				close(original_stdout);
 				if (pid_wait < 0)
 				{
 					perror("waitpid");
@@ -255,6 +314,7 @@ int main(void) {
 				fg = 0;
 			}
 		}
+		
 	}
 
 	return 0;
